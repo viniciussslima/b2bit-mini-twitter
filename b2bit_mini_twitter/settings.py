5a59ad3d5f211db49459f7e9258d581133db176f
Vinicius Santos <vinicius@b2bit.company>
2022-03-17 15:14:07 -0300

    Setup database

--- b2bit_mini_twitter/settings.py
+++ b2bit_mini_twitter/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "b2bit_mini_twitter.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mini_twitter",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
