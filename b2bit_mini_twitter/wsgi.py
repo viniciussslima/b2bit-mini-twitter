0a83d5923b41bf5f6c9b61c32c4ba54cff3d156d
Vinicius Santos <vinicius@b2bit.company>
2022-03-17 15:06:00 -0300

    Add code formatter

--- b2bit_mini_twitter/wsgi.py
+++ b2bit_mini_twitter/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'b2bit_mini_twitter.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "b2bit_mini_twitter.settings")
 
 application = get_wsgi_application()
